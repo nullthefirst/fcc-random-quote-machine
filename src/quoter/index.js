470ad0e7e7ebed390195046fbb3b8756678e2625
Ninte <ninte.dev@gmail.com>
2021-07-23 18:45:55 +0100

    Project setup completed.

---
+++ src/quoter/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Quoter extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Quote machine</h1>
+      </div>
+    );
+  }
+}
+
+export default Quoter;
