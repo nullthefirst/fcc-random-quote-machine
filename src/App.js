7823228410c2d3784ec24748330bc2a0e01b36fa
Ninte <ninte.dev@gmail.com>
2021-07-24 09:37:28 +0100

    Changed core css id name.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="app">
       <ReactFCCtest />
-      <div id="content">
+      <div id="quote-block">
         <Quoter />
         <Footer />
       </div>
